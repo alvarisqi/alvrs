f3d0a797c3ddac39c434dcf20e215a021852db20
alvarisqi <101438298+alvarisqi@users.noreply.github.com>
2022-05-29 21:02:53 +0800

    Delete modul6/PRAK601 directory

--- modul6/PRAK601/Koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Koneksi database prosedural mysqli
-$koneksi = mysqli_connect('localhost', 'root', '', 'perpustakaan');
-
-//Memeriksa koneksi
-if(!$koneksi){
-    echo 'Connection error: '. mysqli_connect_error();
-}
-?>
--- modul6/PRAK601/Login.php
+++
@@ -1,9 +0,0 @@
-<?php
-function cekSessionLogin(){
-    session_start();
-
-    if (!isset($_SESSION["login"])) {
-        header("Location: ErrorPage.php");
-        exit;
-    }
-}
